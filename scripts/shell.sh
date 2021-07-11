cc591288c9d459957e4e1913aa1c6b471b84d35e
ZhengHe <ranchardzheng@gmail.com>
2021-07-11 16:37:52 +0800

    Update shell.sh

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -5,4 +5,4 @@ docker run -it \
            --user "$(id -u):$(id -g)" \
            -v $(pwd):/app \
            -p 5005:5005 \
-           rasa/rasa:$RASA_VERSION-full shell --endpoints=endpoints-local.yml --credentials=credentials-local.yml --debug
+           rasa/rasa:$RASA_VERSION-full shell --endpoints=endpoints-local.yml --credentials=credentials-local.yml $1
